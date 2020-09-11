f86b467302a2880346314cd946eeeb0cab3f477d
Grumblefoot <iceingdeath@msn.com>
2020-09-11 13:46:26 -0700

    FizzBuzzJava - First Entry
    Practice java by creating a FizzBuzz program. This program lets the user choose two diffrent numbers between 2-9 and then finds all numbers between 1-100 that are divisable by one or both of the numbers

---
+++ practiceFizzBuzz/src/practiceFizzBuzz/FizzBuzz.java
@@ -0,0 +1,69 @@
+package practiceFizzBuzz;
+
+import java.util.Scanner;
+
+public class FizzBuzz {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		runFizzBuzz(inputSelect(s));
+	}	
+	
+static Scanner s = new Scanner(System.in);
+	
+	
+	public static void runFizzBuzz(int[] numbers) {
+		
+		int current = 1;
+		while(current < 100) {
+			if(current % numbers[0] == 0 && current % numbers[1] == 0) {
+				System.out.println("FizzBuzz!");
+			}
+			else if(current % numbers[0]== 0) {
+				System.out.println("Fizz");
+			}
+			else if(current % numbers[1] == 0) {
+				System.out.println("Buzz");
+			}
+			else {
+				System.out.println(current);
+			}
+			current++;
+		} 
+	}
+	
+	public static int[] inputSelect(Scanner s) {
+		int[] numbers = {0, 0};
+		int currentNum = 10;
+		
+		for (int i = 0; i < numbers.length; i++) {
+			while(numbers[i] != currentNum) {
+				System.out.println("Please choose a number between 2 - 9");
+				s.nextLine();
+				if(s.hasNextInt()) {
+					currentNum = s.nextInt();
+					if(currentNum > 9 && currentNum <= 1){
+						System.out.println("Not a valid choice!");
+					}
+					else if(i == 1 && currentNum == numbers[0]) {
+						System.out.println("Cannot choose the same number twice!");
+					}
+					else {
+						numbers[i] = currentNum;
+						System.out.println("You choose " + numbers[i]);
+					}
+				}
+				else {
+					System.out.println("Not a valid choice!");
+				}
+			}
+			
+			
+			System.out.println(numbers[0]);
+			System.out.println(numbers[1]);
+		}
+		return numbers;
+	}
+}
+
